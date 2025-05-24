3692e92dcdd743fad393a8f63d062030d57304fa
Stefan2114 <stefan.toma021@gmail.com>
2025-05-24 14:57:24 +0300

    feat: http2 protocol enable

--- backend/kamehoot-backend/src/main/java/com/kamehoot/kamehoot_backend/controllers/HomeController.java
+++ backend/kamehoot-backend/src/main/java/com/kamehoot/kamehoot_backend/controllers/HomeController.java
@@ -1,39 +1,53 @@
-// package com.kamehoot.kamehoot_backend.controllers;
+package com.kamehoot.kamehoot_backend.controllers;
 
-// import java.security.Principal;
+import java.security.Principal;
 
-// import org.springframework.security.access.prepost.PreAuthorize;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.core.io.FileSystemResource;
-// import org.springframework.core.io.Resource;
-// import org.springframework.http.HttpHeaders;
-// import org.springframework.http.MediaType;
-// import org.springframework.http.MediaTypeFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class HomeController {
+import com.kamehoot.kamehoot_backend.services.IQuestionService;
 
-// @GetMapping
-// public String home(Principal principal) {
-// return "Welcome " + principal.getName();
-// }
+import jakarta.servlet.http.HttpServletRequest;
 
-// @PreAuthorize("hasAuthority('SCOPE_read')")
-// @GetMapping("/secure")
-// public String secure(Principal principal) {
-// return "Secure endpoint";
-// }
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 
-// @Override
-// @GetMapping("/intro-video")
-// public ResponseEntity<Resource> getIntroVideo() {
+@RestController
+public class HomeController {
 
-// FileSystemResource video = this.questionService.getIntroVideo();
-// System.out.println(video.getFilename());
-// return ResponseEntity.ok()
-// .header(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=video.mp4")
-// .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
-// .body(video);
-// }
-// }
+    private final IQuestionService questionService;
+
+    public HomeController(IQuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public String home(Principal principal) {
+        return "Welcome " + principal.getName();
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_read')")
+    @GetMapping("/secure")
+    public String secure(Principal principal) {
+        return "Secure endpoint";
+    }
+
+    @GetMapping("/intro-video")
+    public ResponseEntity<Resource> getIntroVideo() {
+        FileSystemResource video = this.questionService.getIntroVideo();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=video.mp4")
+                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .body(video);
+    }
+
+    @GetMapping("/protocol")
+    public String getProtocol(HttpServletRequest request) {
+        return "Protocol: " + request.getProtocol();
+    }
+}
