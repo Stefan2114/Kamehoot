dccd63600114e312ecd354540e835e95f1d73f63
Stefan2114 <stefan.toma021@gmail.com>
2025-05-25 12:23:31 +0300

    all questions in json file

--- backend/kamehoot-backend/src/main/java/com/kamehoot/kamehoot_backend/services/QuestionService.java
+++ backend/kamehoot-backend/src/main/java/com/kamehoot/kamehoot_backend/services/QuestionService.java
@@ -45,7 +45,7 @@ public class QuestionService implements IQuestionService {
                 this.passwordEncoder = passwordEncoder;
                 this.userRepository = userRepository;
 
-                // saveJsonQuestions();
+                saveJsonQuestions();
         }
 
         @Override
