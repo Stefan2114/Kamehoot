e8c81a5224359f1c20052fbdf1acffc702f83c78
Stefan2114 <stefan.toma021@gmail.com>
2025-07-02 10:52:06 +0300

    fix: docker compose working

--- backend/kamehoot-backend/src/main/java/com/kamehoot/kamehoot_backend/services/DBService.java
+++ backend/kamehoot-backend/src/main/java/com/kamehoot/kamehoot_backend/services/DBService.java
@@ -1,8 +1,6 @@
 package com.kamehoot.kamehoot_backend.services;
 
 import java.io.InputStream;
-import java.time.ZonedDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -14,7 +12,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kamehoot.kamehoot_backend.DTOs.QuestionDTO;
-import com.kamehoot.kamehoot_backend.enums.Visibility;
 import com.kamehoot.kamehoot_backend.models.AppUser;
 import com.kamehoot.kamehoot_backend.models.Category;
 import com.kamehoot.kamehoot_backend.models.Question;
@@ -39,57 +36,45 @@ public class DBService {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
 
-        // saveJsonQuestions();
+        saveJsonQuestions();
     }
 
     private void saveJsonQuestions() {
 
-        Category category1 = new Category();
-        category1.setName("Math");
-        Category category2 = new Category();
-        category2.setName("Football");
+        try {
+            Category category1 = new Category();
+            category1.setName("Math");
+            Category category2 = new Category();
+            category2.setName("Football");
 
-        AppUser user = new AppUser();
-        user.setUsername("admin");
-        user.setPassword(passwordEncoder.encode("Admin1234!"));
-        user.setRoles(Set.of("ADMIN"));
-        AppUser admin = userRepository.save(user);
+            this.categoryRepository.save(category1);
+            this.categoryRepository.save(category2);
 
-        AppUser user2 = new AppUser();
-        user2.setUsername("stef");
-        user2.setPassword(passwordEncoder.encode("Stefan2004!"));
-        user2.setRoles(Set.of("USER"));
-        userRepository.save(user2);
+            AppUser user = new AppUser();
+            user.setUsername("admin");
+            user.setPassword(passwordEncoder.encode("Admin1234!"));
+            user.setRoles(Set.of("ADMIN"));
+            AppUser admin = userRepository.save(user);
 
-        Category mathCategory = this.categoryRepository.save(category1);
-        this.categoryRepository.save(category2);
+            List<QuestionDTO> jsonQuestions = loadQuestionsFromJson();
 
-        Question userQuestion = new Question();
-        userQuestion.setCreator(user2);
-        userQuestion.setVisibility(Visibility.PRIVATE);
-        userQuestion.setCreationDate(ZonedDateTime.now());
-        userQuestion.setCategory(mathCategory);
-        userQuestion.setCorrectAnswer("64");
-        userQuestion.setDifficulty(1);
-        userQuestion.setQuestionText("What is 8*8?");
-        userQuestion.setWrongAnswers(Arrays.asList("81", "56"));
-        this.questionRepository.save(userQuestion);
+            for (QuestionDTO question : jsonQuestions) {
+                Category category = this.categoryRepository.findByName(question.category());
+                Question newQuestion = new Question();
+                newQuestion.setCreator(admin);
+                newQuestion.setCreationDate(question.creationDate());
+                newQuestion.setCategory(category);
+                newQuestion.setCorrectAnswer(question.correctAnswer());
+                newQuestion.setDifficulty(question.difficulty());
+                newQuestion.setQuestionText(question.questionText());
+                newQuestion.setWrongAnswers(question.wrongAnswers());
+                this.questionRepository.save(newQuestion);
 
-        List<QuestionDTO> jsonQuestions = loadQuestionsFromJson();
-
-        for (QuestionDTO question : jsonQuestions) {
-            Category category = this.categoryRepository.findByName(question.category());
-            Question newQuestion = new Question();
-            newQuestion.setCreator(admin);
-            newQuestion.setCreationDate(question.creationDate());
-            newQuestion.setCategory(category);
-            newQuestion.setCorrectAnswer(question.correctAnswer());
-            newQuestion.setDifficulty(question.difficulty());
-            newQuestion.setQuestionText(question.questionText());
-            newQuestion.setWrongAnswers(question.wrongAnswers());
-            this.questionRepository.save(newQuestion);
-
+            }
+        } catch (Exception e) {
+            System.err.println("Entities are already saved on the db");
         }
+
     }
 
     private List<QuestionDTO> loadQuestionsFromJson() {
